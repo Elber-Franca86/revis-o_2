526b59437e2a4d9c4bf253b6dfd609ef07e65651
Elber C. da França <ellberfranca86@gmail.com>
2024-04-01 20:25:05 -0300

    Resolve questão 2

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -3,3 +3,13 @@ def saudar():
     print("Olá. ", nome,"! Seja bem-vindo(a)!")
 
 saudar()
+
+
+def somar():
+    primeiro_numero = int(input("Digite o primeiro numero")) #acrescenta p int para
+    segundo_numero = int(input("Digite o segundo numero"))   #indicar que não é mais uma string
+    soma = primeiro_numero + segundo_numero
+
+    print("A soma dos numeros é: ", soma)
+
+somar()
