46b53a9a6e1167e8f410f4b3204aff43dd9032b2
Elber C. da França <ellberfranca86@gmail.com>
2024-04-01 20:47:59 -0300

    Resolve questão 4

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -6,8 +6,8 @@ saudar()
 
 
 def somar():
-    primeiro_numero = int(input("Digite o primeiro numero ")) #acrescenta p int para
-    segundo_numero = int(input("Digite o segundo numero "))   #indicar que não é mais uma string
+    primeiro_numero = int(input("Digite o primeiro numero: ")) #acrescenta p int para
+    segundo_numero = int(input("Digite o segundo numero: "))   #indicar que não é mais uma string
     soma = primeiro_numero + segundo_numero
 
     print("A soma dos numeros é: ", soma)
@@ -15,10 +15,19 @@ def somar():
 somar()
 
 def multiplicar():
-    num_1 = int(input ("Digite o primeiro numero "))
-    num_2 = int(input("Digite o segundo numero "))
+    num_1 = int(input ("Digite o primeiro numero: "))
+    num_2 = int(input("Digite o segundo numero: "))
     multiplos = num_1 * num_2
 
     print("O produto dos numeros é: ", multiplos)
 
 multiplicar()
+
+
+def divisao():
+    n1 = int(input("Digite um numero: "))
+    n2 = int(input("Digite o segundo numero: "))
+    dividendo = n1 / n2
+    print("A divisão do numero", str(n1),  "por dois é: ", dividendo)
+
+divisao()
