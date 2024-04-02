650f34446ef608ea196fe7f3c83de14c5315be97
Elber C. da França <ellberfranca86@gmail.com>
2024-04-01 21:29:07 -0300

    resolve questão 7

--- condicionais.py
+++ condicionais.py
@@ -6,3 +6,13 @@ def verifica_idade():
         print("É menor de idade")
 
 verifica_idade()
+
+def par_impar():
+
+    numero = int(input("Digite um número: "))
+    if (numero % 2 == 0):
+        print("O número", str(numero), "é par.")
+    else:
+        print("O numero", str(numero), "é impar.")
+
+par_impar()
